a34c55cbb489bc081e482c1bdafda38745fa2a9e
Leonardo / LuKi <leo.monteiro06@live.com>
2021-04-10 19:16:48 -0300

    Small improvement

--- dwm/config.h
+++ dwm/config.h
@@ -15,7 +15,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=9", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "sans:size=9", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#ffffff";
 static char normfgcolor[]           = "#ffffff";
