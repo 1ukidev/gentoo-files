89b11591105fac0c1712453aaea16e5964b9eb45
Leonardo Monteiro <leo.monteiro06@live.com>
2023-06-27 16:59:20 -0300

    Fix indent size

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"Screen: ", "echo $(light -G)%", 10, 0},
-  {"Vol: ", "amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'", 5, 0},
+  	{"Screen: ", "echo $(light -G)%", 10, 0},
+  	{"Vol: ", "amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }'", 5, 0},
 	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 15, 0},
 	{"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
 	{"", "echo 'dwm 6.4 '", 0, 0},
