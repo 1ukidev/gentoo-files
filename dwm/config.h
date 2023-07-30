a43daf4542a596d547f9e6c604f9d250fd5653b5
Leonardo Monteiro <leo.monteiro06@live.com>
2023-07-30 17:39:57 -0300

    dwm: change resizehints

--- dwm/config.h
+++ dwm/config.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -106,9 +106,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
   	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,                            XF86XK_AudioMute, spawn, {.v = mutevol } },
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-  	{ 0,                            XF86XK_MonBrightnessUp,	spawn, {.v = light_up} },
-	{ 0,                            XF86XK_MonBrightnessDown, spawn, {.v = light_down} },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol } },
+  	{ 0,                            XF86XK_MonBrightnessUp, spawn, {.v = light_up } },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn, {.v = light_down } },
 };
 
 /* button definitions */
